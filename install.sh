47ffaee456b6d5865bed723f2fd53d43d409f412
Hung Nguyen <hachi.codingcat@gmail.com>
2023-04-15 07:15:01 -0700

    Add tmux & zshrc & tmux-sessionizer

---
+++ install.sh
@@ -0,0 +1,6 @@
+cd ~
+ln -s $HOME/.dotfiles/.config/nvim $HOME/.config
+ln -s $HOME/.dotfiles/.config/scripts $HOME/.config
+
+ln -s $HOME/.dotfiles/.tmux.conf $HOME
+ln -s $HOME/.dotfiles/.zshrc $HOME
